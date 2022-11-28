e96c510ad3b0c3bc958384fec4794425ad1fefe0
Grigory <grishka5556@gmail.com>
2022-11-28 10:08:43 +0300

    edit

--- notes.controller.js
+++ notes.controller.js
@@ -41,8 +41,19 @@ async function removeNote(id) {
     const filtereds = notes.filtered(note => note.id !== id)
     await saveNotes(filtereds)
     console.log(chalk.red(`Note with id='${id}' has been removed.`))
-  }
+}
+
+
+async function updateNote(noteData) {
+    const notes = await getNotes()
+    const index = notes.findIndex(note => note.id === noteData.id)
+    if (index >= 0) {
+        notes[index] = { ...notes[index], ...noteData }
+        await saveNotes(notes)
+        console.log(chalk.bgGreen(`Note with id="${noteData.id}" has been updated`))
+    }
+}
 
 module.exports = {
-    addNote, getNotes, removeNote
+    addNote, getNotes, removeNote, updateNote
 }
